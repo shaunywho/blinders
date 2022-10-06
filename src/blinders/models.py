507ae726213a7298b2dc586e20d61b68555ef17f
Shaun <shaunhohoho@gmail.com>
2022-10-06 13:42:24 +0100

    switching branches

--- src/blinders/models.py
+++ src/blinders/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-  ip_address = models.CharField(max_length=20, null=True)
+  # ip_address = models.CharField(max_length=20, null=True)
   age = models.IntegerField(null=True, blank=False)
-  profile_picture_url = models.ImageField(null = True, blank = True, upload_to = "images/")
-  blurred_profile_picture_url = models.ImageField(null = True, blank = True,upload_to = "images/")
-  description = models.TextField(default = "Description")
+  # profile_picture_url = models.ImageField(null = True, blank = True, upload_to = "images/")
+  # blurred_profile_picture_url = models.ImageField(null = True, blank = True,upload_to = "images/")
+  # description = models.TextField(default = "Description")
+
+class Profile(models.Model):
+  
+  # ip_address = models.CharField(max_length=20, null=True)
+  # profile_picture_url = models.ImageField(null = True, blank = True, upload_to = "images/")
+  # blurred_profile_picture_url = models.ImageField(null = True, blank = True,upload_to = "images/")
+  # description = models.TextField(default = "Description")
+
 
 
     
